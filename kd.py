1745a867f67030f56e714d3e9066ed0baeef934c
sudiptob <sudiptob@qti.qualcomm.com>
2023-10-18 13:17:09 +0530

    WIP: KD with Alexnet model on CIFAR-10 dataset
    - Added Alexnet model in models.py
    - Modified last layer of Alexnet model with number of classes changed to 10 from 1000 (originally for ImageNet dataset)
    - Modifications of hyperparameters

--- kd.py
+++ kd.py
@@ -1,20 +1,25 @@
+import random
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torchvision import transforms
-from models import CustomNet, SmallNet
+from torch.utils.data import SubsetRandomSampler
+from models import LeNet, SmallNet, AlexNet
+from torchsummary import summary
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def compute_accuracy(model, loader, device):
-    model.to(device)
-    model.eval()
-
     correct = 0
     total = 0
-
+    
+    model.eval()
     with torch.no_grad():
-        for inputs, labels in loader:
+        for idx, (inputs, labels) in enumerate(loader):
             inputs, labels = inputs.to(device), labels.to(device)
 
             outputs = model(inputs)
@@ -27,29 +32,55 @@ def compute_accuracy(model, loader, device):
     return accuracy
     
     
-def download(data_root, batch_size):
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+def downloadCIFAR10(data_root, batch_size):
+    train_transform = transforms.Compose([transforms.Resize((70, 70)),
+                                          transforms.RandomCrop((64, 64)),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    
+    test_transform = transforms.Compose([transforms.Resize((64, 64)),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
 
     trainset = torchvision.datasets.CIFAR10(root=data_root, train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                            shuffle=True, num_workers=2)
-
+                                            download=True, transform=train_transform)
+    
     testset = torchvision.datasets.CIFAR10(root=data_root, train=False,
-                                        download=True, transform=transform)
+                                           download=True, transform=test_transform)
+
+    trainloader = torch.utils.data.DataLoader(dataset=trainset,
+                                              batch_size=batch_size,
+                                              num_workers=8,
+                                              shuffle=True,
+                                              drop_last=True)
+    
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                            shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=8)
 
     return trainloader, testloader
 
 
-def train(net, trainloader, testloader, num_epochs, learning_rate, momentum, device):
+def downloadMNIST(data_root, batch_size):
+    trainset = torchvision.datasets.MNIST(root=data_root, train=True,
+                                            download=True, transform=transforms.ToTensor())
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=8)
+
+    testset = torchvision.datasets.MNIST(root=data_root, train=False,
+                                        download=True, transform=transforms.ToTensor())
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=8)
+
+    return trainloader, testloader
+
+
+def train(net, trainloader, validloader, num_epochs, learning_rate, momentum, device, tensorboard_writer):
     print(f'Total number of parameters in network: {sum(p.numel() for p in net.parameters())}')
     
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+    # optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)  
     
     loss_vals = []
     train_accuracies = []
@@ -59,6 +90,9 @@ def train(net, trainloader, testloader, num_epochs, learning_rate, momentum, dev
         running_loss = 0.0
         
         # Iterate over each batch
+        # Set model to train
+        net.train()
+        
         for idx, data in enumerate(trainloader):
             features, labels = data[0].to(device), data[1].to(device)
             
@@ -78,14 +112,34 @@ def train(net, trainloader, testloader, num_epochs, learning_rate, momentum, dev
             optimizer.step()
             
             running_loss += loss.item()
+            
+            '''
+            if idx % 20 == 19:
+                # ...log the running loss
+                tensorboard_writer.add_scalar('training loss',
+                                running_loss / 20,
+                                epoch * len(trainloader) + idx)
+
+                # ...log a Matplotlib Figure showing the model's predictions on a
+                # random mini-batch
+                tensorboard_writer.add_figure('predictions vs. actuals',
+                                plot_classes_preds(net, features, labels),
+                                global_step=epoch * len(trainloader) + idx)
+                
+                running_loss = 0.0
+            '''
+                
         
         epoch_loss = running_loss / len(trainloader)
         print(f"Epoch {epoch + 1} / {num_epochs}, Loss: {epoch_loss}")
         
         # Compute accuracy after each epoch
+        # Set model to eval
+        net.eval()
+        
         train_accuracy = compute_accuracy(net, trainloader, device)
         test_accuracy = compute_accuracy(net, testloader, device)
-        print(f'Epoch {epoch + 1} finished --> train accuracy {train_accuracy} test accuracy {test_accuracy}')
+        print(f'Epoch {epoch + 1} finished --> train accuracy {train_accuracy} validation accuracy {test_accuracy}')
         
         loss_vals.append(epoch_loss)
         train_accuracies.append(train_accuracy)
@@ -99,8 +153,143 @@ def train(net, trainloader, testloader, num_epochs, learning_rate, momentum, dev
     }
     
     return params
+
+
+def train_kd(teacher_model, student_model, trainloader, testloader, num_epochs, learning_rate, momentum, device):
+    print(f'Total number of parameters in teacher network: {sum(p.numel() for p in teacher_model.parameters())}')
+    print(f'Total number of parameters in student network: {sum(p.numel() for p in student_model.parameters())}')
     
+    criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.SGD(student_model.parameters(), lr=learning_rate, momentum=momentum)
+    optimizer = torch.optim.Adam(student_model.parameters(), lr=learning_rate)
     
+    loss_vals = []
+    train_accuracies = []
+    test_accuracies = []
+    
+    teacher_model.eval()
+    student_model.train()
+    
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        
+        # Iterate over each batch
+        for idx, data in enumerate(trainloader):
+            features, labels = data[0].to(device), data[1].to(device)
+            
+            # Initialize optimizer
+            optimizer.zero_grad()
+            
+            # Forward pass features through net to get teacher predictions
+            with torch.no_grad():
+                teacher_preds = teacher_model(features)
+            
+            # Forward pass features through net to get student predictions
+            student_preds = student_model(features)
+            
+            soft_targets = nn.functional.softmax(teacher_preds / 2, dim=-1)
+            soft_preds = nn.functional.softmax(student_preds / 2, dim=-1)
+            
+            soft_loss = torch.sum(soft_targets * soft_preds) / soft_preds.size()[0] * 4
+            label_loss = criterion(soft_preds, labels)
+            
+            loss = 0.25 * soft_loss + 0.75 * label_loss
+            
+            # Backpropagate loss through net
+            loss.backward()
+            
+            # Step through optimizer
+            optimizer.step()
+            
+            running_loss += loss.item()
+        
+        epoch_loss = running_loss / len(trainloader)
+        print(f"Epoch {epoch + 1} / {num_epochs}, Loss: {epoch_loss}")
+        
+        # Compute accuracy after each epoch
+        train_accuracy = compute_accuracy(student_model, trainloader, device)
+        test_accuracy = compute_accuracy(student_model, testloader, device)
+        print(f'Epoch {epoch + 1} finished --> train accuracy {train_accuracy} test accuracy {test_accuracy}')
+        
+        loss_vals.append(epoch_loss)
+        train_accuracies.append(train_accuracy)
+        test_accuracies.append(test_accuracy)
+        
+    params = {
+        'Model' : student_model,
+        'Epoch_losses' : loss_vals,
+        'Train_accuracies' : train_accuracies,
+        'Test_accuracies' : test_accuracies
+    }
+    
+    return params
+
+
+#### helper functions
+
+# helper function to show an image
+# (used in the `plot_classes_preds` function below)
+def matplotlib_imshow(img, one_channel=False):
+    img = img.to(torch.device('cpu'))
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.to(torch.device('cpu')).numpy())
+    return preds, [nn.functional.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx], one_channel=True)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+                    color=("green" if preds[idx]==labels[idx].item() else "red"))
+    return fig
+
+
+def set_all_seeds(seed):
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def set_deterministic():
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+    torch.set_deterministic(True)
+
+
 if __name__ == '__main__':
     # Download dataset
     # Create train, validation and test splits and batches
@@ -109,26 +298,47 @@ if __name__ == '__main__':
     # Initialize optimizer
     # Loop over each batch
     
+    random_seed = 1
     data_root = './data'
-    batch_size = 128
-    num_epochs = 100
-    learning_rate = 0.001
+    batch_size = 64
+    num_epochs = 200
+    learning_rate = 0.0001
     momentum = 0.9
+    tensorboard_log_dir = 'runs'
+    run_suffix = 'cifar_10_experiments'
+    
+    set_all_seeds(random_seed)
     
     classes = ('plane', 'car', 'bird', 'cat', 
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     
+    num_classes = len(classes)
+    
+    tensorboard_writer = SummaryWriter(os.path.join(tensorboard_log_dir, run_suffix))
+    
     device = torch.device('cuda')
     
-    trainloader, testloader = download(data_root, batch_size)
-    
+    trainloader, testloader = downloadCIFAR10(data_root, batch_size)
+    # trainloader, testloader = downloadMNIST(data_root, batch_size)
+
     # Train big network
-    big_net = CustomNet().to(device)
-    big_train_info = train(big_net, trainloader, testloader, num_epochs, learning_rate, momentum, device)
+    # lenet = LeNet().to(device)
+    
+    torch.manual_seed(random_seed)
+    
+    alexnet = AlexNet(num_classes).to(device)
+    
+    alexnet_train_info = train(alexnet, trainloader, testloader, num_epochs, learning_rate, momentum, device, tensorboard_writer)
     
     # Optionally save trained model weights
     small_net = SmallNet().to(device)
+    
+    # Create another copy small_net to train using KD
+    small_net_kd = SmallNet().to(device)
+    
     small_train_info = train(small_net, trainloader, testloader, num_epochs, learning_rate, momentum, device)
     print(f'Total number of parameters in network: {sum(p.numel() for p in small_net.parameters())}')
+    
+    kd_train_info = train_kd(alexnet, small_net_kd, trainloader, testloader, num_epochs, learning_rate, momentum, device)
     
     print('Done')
--- models.py
+++ models.py
@@ -2,21 +2,81 @@ import torch
 import torch.nn as nn
 
 
-class CustomNet(nn.Sequential):
+class LeNet(nn.Sequential):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 10, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
-        self.bn1 = nn.BatchNorm2d(10)
-        self.conv2 = nn.Conv2d(10, 16, 3)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, dilation=1, ceil_mode=False),
+            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, dilation=1, ceil_mode=False),
+            nn.Conv2d(in_channels=192, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False),
+        )
+        
+        self.adaptive_avg_pool = nn.AdaptiveAvgPool2d((6, 6))
+        self.fc = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=256*6*6, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=4096, out_features=1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=1024, out_features=num_classes),
+        )
+        
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.adaptive_avg_pool(x)
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc(x)
+        preds = nn.functional.softmax(x, dim=1)
+        return preds
+
+
+
+class LeNetBatchNorm(nn.Sequential):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.bn2 = nn.BatchNorm2d(16)
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.bn3 = nn.BatchNorm1d(120)
         self.fc2 = nn.Linear(120, 84)
         self.bn4 = nn.BatchNorm1d(84)
         self.fc3 = nn.Linear(84, 10)
-
+    
     def forward(self, x):
         x = self.bn1(self.pool(self.relu(self.conv1(x))))
         x = self.bn2(self.pool(self.relu(self.conv2(x))))
@@ -25,8 +85,8 @@ class CustomNet(nn.Sequential):
         x = self.bn4(self.fc2(x))
         x = self.fc3(x)
         return x
-    
-    
+
+
 class SmallNet(nn.Sequential):
     def __init__(self):
         super().__init__()
