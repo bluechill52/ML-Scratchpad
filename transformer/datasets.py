55d8271b03b51fd1379fec80a29f64ad427496b4
sudiptob <sudiptob@qti.qualcomm.com>
2024-05-06 15:32:35 +0530

    WIP: Added transformer model - added feedforward and sequence of transformer layers
    - Added function to create torch dataloader for custom dataset
    - Added extra config params

--- transformer/datasets.py
+++ transformer/datasets.py
@@ -17,8 +17,8 @@ class DatasetV1(Dataset):
         # Create input ids and target ids using sliding window of context size and stride
         start_idx = 0
         for start_idx in range(0, len(token_ids) - context_size, stride):
-            self.input_ids.append(token_ids[start_idx:start_idx+context_size])
-            self.target_ids.append(token_ids[start_idx + 1:start_idx+context_size + 1])
+            self.input_ids.append(torch.tensor(token_ids[start_idx:start_idx+context_size]))
+            self.target_ids.append(torch.tensor(token_ids[start_idx + 1:start_idx+context_size + 1]))
     
     def __len__(self):
         return len(self.input_ids)
@@ -32,16 +32,33 @@ class DatasetV1(Dataset):
             print(target_id)
 
 
+def createDataloaderV1(text, tokenizer, batch_size=4, context_size=256, stride=128, shuffle=True, drop_last=True, num_workers=8):
+    # Create dataset
+    dataset = DatasetV1(text, tokenizer, context_size, stride)
+
+    # Create dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
 
 if __name__ == '__main__':
     fname = 'the-verdict.txt'
     context_size = 5
     stride = 1
+    batch_size = 1
 
-    Tokenizer = SimpleTokenizer(fname)
+    tokenizer = SimpleTokenizer(fname)
 
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), fname), 'r', encoding='utf-8') as f:
         raw_text = f.read()
 
-    dataset = DatasetV1(raw_text, Tokenizer, context_size, stride)
+    dataset = DatasetV1(raw_text, tokenizer, context_size, stride)
+    dataloader = createDataloaderV1(raw_text, tokenizer, batch_size, context_size, stride)
     dataset.print()
--- transformer/transformer_network1.py
+++ transformer/transformer_network1.py
@@ -28,7 +28,7 @@ class MultiHeadAttentionBlock(nn.Module):
         
         self.num_heads = num_heads
         self.MHABlock = nn.ModuleList([AttentionBlock(in_channels, out_channels) for _ in range(num_heads)])
-        self.wo = nn.Linear(out_channels, in_channels)
+        self.wo = nn.Linear(len(self.MHABlock) * out_channels, in_channels)
     
     def forward(self, x):
         z = torch.cat([head(x) for head in self.MHABlock], 1)
@@ -43,17 +43,89 @@ class TransformerLayer(nn.Module):
         self.norm = nn.LayerNorm(in_channels)
         
     def forward(self, x):
-        return self.norm(x + mha(x))
+        return self.norm(x + self.mha(x))
+
+
+class Feedforward(nn.Module):
+    def __init__(self, in_channels, hidden_channels):
+        super(Feedforward, self).__init__()
+        
+        self.layer1 = nn.Linear(in_channels, hidden_channels)
+        self.activation = nn.GELU()
+        self.layer2 = nn.Linear(hidden_channels, in_channels)
     
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.activation(x)
+        return self.layer2(x)
 
-NUM_SAMPLES = 5
-EMBEDDING_DIM = 10
-KEY_DIM = 256
-QUERY_DIM = 256
-VALUE_DIM = 256
-NUM_HEADS = 4
+        
+class TransformerEncoder(nn.Module):
+    def __init__(self, in_channels, ff_hidden_channels, out_channels, num_heads, num_layers):
+        super(TransformerEncoder, self).__init__()
+        
+        self.num_layers = num_layers
+        
+        self.encoder = nn.Sequential(
+            *[TransformerLayer(in_channels, out_channels, num_heads) for _ in range(num_layers)]
+        )
+        
+        self.ff = Feedforward(in_channels, ff_hidden_channels)
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        return self.ff(x) + x
 
-x = torch.randn((NUM_SAMPLES, EMBEDDING_DIM))
-m = MultiHeadAttentionBlock(EMBEDDING_DIM, KEY_DIM, NUM_HEADS)
-print(x)
-print(m(x).shape)
+
+class Model(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 ff_hidden_channels,
+                 attention_out_channels,
+                 num_attention_heads,
+                 num_transformer_layers):
+        super(Model, self).__init__()
+        
+        # Embedding layer - nn.Linear
+        self.input_embedding = nn.Linear(in_channels, in_channels)
+        self.pos_embedding = nn.Linear(in_channels, in_channels)
+        
+        # Transformer encoder
+            # N transformer layers
+                # M Multi-head self-attention blocks
+                    # Self-attention block
+                # Layer norm
+                # Residual (o/p + i/p)
+            # Feedforward
+                # Linear
+                # Activation function
+                # Linear
+        self.encoder = TransformerEncoder(in_channels,
+                                          ff_hidden_channels,
+                                          attention_out_channels,
+                                          num_attention_heads,
+                                          num_transformer_layers)
+    
+    def forward(self, x):
+        x = self.input_embedding(x) + self.pos_embedding(x)
+        x = self.encoder(x)
+        return x
+        
+
+
+if __name__ == '__main__':
+    # Configuration
+    NUM_SAMPLES = 5
+    EMBEDDING_DIM = 10
+    KEY_DIM = 256
+    FF_HIDDEN_DIM = 64
+    QUERY_DIM = 256
+    VALUE_DIM = 256
+    NUM_HEADS = 4
+    NUM_TRANSFORMER_LAYERS = 4
+
+    x = torch.randn((NUM_SAMPLES, EMBEDDING_DIM))
+    print(x)
+
+    M = Model(EMBEDDING_DIM, FF_HIDDEN_DIM, KEY_DIM, NUM_HEADS, NUM_TRANSFORMER_LAYERS)
+    print(M(x).shape)
